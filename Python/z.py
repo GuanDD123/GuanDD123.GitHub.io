5d40fe3cc9f0b5b200107b2a0a20a4fa99426bb6
GuanDD123 <guandongsika@gmail.com>
2024-11-08 16:43:43 +0800

    i

--- Python/z.py
+++ Python/z.py
@@ -1,4 +1,16 @@
-try:
-    3/0
-except ZeroDivisionError:
-    raise ValueError
+import os
+import os.path
+
+dir_ = os.getcwd()
+dir_list = os.listdir(dir_)
+f_list = []
+for f in dir_list:
+    if os.path.isdir(f):
+        for ff in os.listdir(f):
+            f_list.append('<li><a href="./显示code.html?param=' + os.path.join('Python', f, ff) + '">' + ff + '</a></li>')
+    else:
+        f_list.append('<li><a href="./显示code.html?param=' + os.path.join('Python', f) + '">' + f + '</a></li>')
+for i in f_list:
+    print(i)
+
+input()
