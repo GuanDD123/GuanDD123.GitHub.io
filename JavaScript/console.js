7ccd120b0d71ba06044a915a4a8d5078c3087bbf
Guan <86156249+GuanDD123@users.noreply.github.com>
2024-11-08 20:31:04 +0800

    Delete JavaScript directory

--- JavaScript/console.js
+++
@@ -1,5 +0,0 @@
-console.log() //输出调试信息
-console.assert() //对输入的表达式进行断言，只有表达式为 False 时，才输出相应的信息到控制台
-console.count() //统计代码被执行的次数
-console.dir() //将 DOM 节点以 DOM 树的结构进行输出，可以查看对象的方法等
-console.trace() //打出 Js 的函数调用栈
